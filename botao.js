2df62db46f59574be10ec9fe5d90c256739e573c
romulosofteng <romulo@lsmarketingestrategico.com.br>
2025-06-15 20:04:42 -0300

    Add files via upload

---
+++ botao.js
@@ -0,0 +1,7 @@
+document.getElementById('botao-js').onclick = function() {
+    document.getElementById('mensagem').style.display = 'block';
+};
+
+document.getElementById('botao-js').onmouseover = function() {
+    this.style.backgroundColor = '#2b5a8a';
+}
